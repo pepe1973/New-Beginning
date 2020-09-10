7679cf8da85ae09c1df22d51b8da064bfb76b662
pepe1973 <bodrogipr@gmail.com>
2020-09-10 09:27:25 +0200

    Harmadik commit

---
+++ alap.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let nev = 'Bodrogi Péter';
+
+/* Modális ablak függvények */
+// window.alert(nev);
+// alert(nev);
+
+// let tanuloSzam = window.prompt('Hány tanuló van jelen:', 12);
+// let szoveg = `A tanulok száma: ${tanuloSzam}`;
+
+let szoveg = 'Hello world';
+// szoveg = null;
+// window.alert(szoveg);
+
+document.getElementById('kiir').innerText = szoveg;
